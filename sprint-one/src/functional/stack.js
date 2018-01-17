dfd003b61dce932ddd07ba96874a4104f8140dcd
Whitney Seiler <whitney.seiler@gmail.com>
2018-01-17 15:07:14 -0800

    Pass all Stack tests

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,18 +1,31 @@
 var Stack = function() {
   var someInstance = {};
-
+  var count = 0;
+  
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    count ++;
+    storage[count] = value;
   };
 
   someInstance.pop = function() {
+    if (count > 0) {
+      var pop = storage[count];
+      delete storage[count];
+      count --;
+      return pop;
+    }
   };
+  // someInstance.pop();
 
   someInstance.size = function() {
+    return count;
   };
 
   return someInstance;
 };
+
+Stack();
