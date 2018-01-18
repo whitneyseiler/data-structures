c1da89b91ebee24dea8ad148a485f0780c502014
Whitney Seiler <whitney.seiler@gmail.com>
2018-01-18 10:42:05 -0800

    Pass all tests

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,25 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.count = 0;
 };
 
+Queue.prototype.enqueue = function(value) {
+  this[this.count++] = value;
+}
 
+Queue.prototype.dequeue = function() {
+  if (this.count > 0) {
+    this.count--;
+  }
+  var deq = this[0];
+  for (var i = 0; i < this.count; i++) {
+    this[i] = this[i+1];
+  }
+  delete this[this.count];
+  return deq;
+}
+
+Queue.prototype.size = function() {
+  return this.count;
+}
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,22 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.count = 0;
 };
 
+Stack.prototype.push = function(value) {
+  this[this.count++] = value;
+}
 
+Stack.prototype.pop = function() {
+  if (this.count > 0) {
+    this.count--;
+  }
+  var pop = this[this.count];
+  delete this[this.count];
+  return pop;
+}
+
+Stack.prototype.size = function(){
+  return this.count;
+}
