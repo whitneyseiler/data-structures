0a7c85b429cf45db04c16a9f85b1e8c446a4f23c
Whitney Seiler <whitney.seiler@gmail.com>
2018-01-18 10:06:49 -0800

    Pass all Stack tests

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,28 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var storage = {
+    count: 0,
+  }
+  _.extend(storage, stackMethods)
+  return storage;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value){
+    this[this.count++] = value;
+  },
 
+  pop: function() {
+    if (this.count > 0 ) {
+      this.count--
+    }
+    var pop = this[this.count];
+    delete this[this.count];
+    return pop;
+  },
 
+  size: function() {
+    return this.count;
+  }
+};
