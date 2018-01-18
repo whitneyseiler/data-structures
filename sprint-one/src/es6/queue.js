dd63d2a893691e9bc7e83e3d348dce0294740340
Whitney Seiler <whitney.seiler@gmail.com>
2018-01-18 10:50:03 -0800

    Pass all tests

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,26 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.count = 0;
   }
 
+  enqueue(value) {
+    this[this.count++] = value;
+  }
+
+  dequeue() {
+    if (this.count > 0) {
+      this.count--;
+    }
+    var deq = this[0];
+    for (var i = 0; i < this.count; i++) {
+      this[i] = this[i+1];
+    }
+    delete this[this.count];
+    return deq;
+  }
+
+  size() {
+    return this.count;
+  }
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,24 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.count = 0;
+  }
+
+  push(value){
+    this[this.count++] = value;
+  }
+
+  pop(){
+    if (this.count > 0) {
+      this.count--
+    }
+    var pop = this[this.count];
+    delete this[this.count];
+    return pop;
+  }
+
+  size(){
+    return this.count;
   }
 
 }
