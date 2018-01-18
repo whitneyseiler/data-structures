89fd8bdb2e94fea5e785919b8be97ad26473fda7
Whitney Seiler <whitney.seiler@gmail.com>
2018-01-18 10:36:56 -0800

    Pass all Stack tests

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,29 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = Object.create(stackMethods);
+  instance.count = 0;
+
+  return instance;
 };
 
-var stackMethods = {};
+var stackMethods = {
 
+  push: function(value) {
+    this[this.count++] = value;
+  },
 
+  pop: function() {
+    if (this.count > 0) {
+      this.count--;
+    }
+    var pop = this[this.count];
+    delete this[this.count];
+    return pop;
+  },
+
+  size: function() {
+    return this.count;
+  }
+
+};
