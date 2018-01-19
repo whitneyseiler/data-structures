84b9d3b866105083f0fc88fff48a7aa918050c11
whitneyseiler <whitney.seiler@gmail.com>
2018-01-19 10:43:40 -0800

    Pass all linkedList tests

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,34 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if (this.head === null) { //if its the first item
+      this.head = newNode;
+      this.tail = newNode;
+    }
+    if (this.tail) { //if there is already a tail
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
   };
 
   list.removeHead = function() {
+    if (this.head) {
+      var value = this.head.value;
+      this.head = this.head.next;
+      return value;
+    }
   };
 
   list.contains = function(target) {
+    var currentNode = this.head;
+    while (currentNode) {
+      if (currentNode.value === target) {
+        return true;
+      }
+      currentNode = currentNode.next;
+    }
+    return false;
   };
 
   return list;
