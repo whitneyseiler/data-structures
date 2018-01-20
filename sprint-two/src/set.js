cdd1d0857e4cbb039f6e92dc2486e75c05b6be94
whitneyseiler <whitney.seiler@gmail.com>
2018-01-20 09:39:19 -0800

    Pass all Set tests

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,29 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
+//takes a string and adds it to the set
 setPrototype.add = function(item) {
+  if (!this._storage.includes(item)) {
+    this._storage.push(item);
+  }
 };
 
+//takes any string and returns boolean if/if not found
 setPrototype.contains = function(item) {
+  return this._storage.includes(item);
 };
 
+//takes string and removes it from a set
 setPrototype.remove = function(item) {
+  this._storage.splice(this._storage.indexOf(item), 1);
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+  O(n)
  */
